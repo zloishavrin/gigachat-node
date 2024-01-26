646e7b72840981714d7801f7d0e4ecd4cf036044
zloishvarin <nexxxusoffical@gmail.com>
2024-01-26 23:20:51 +0500

    add build of new version

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,20 @@
 declare class GigaChat {
-    authorization: string;
+    authorization: string | undefined;
     private clientSecretKey;
     private isIgnoreTSL;
     private isPersonal;
     private autoRefreshToken;
+    private imgOn;
+    private imgPath;
     private url;
     private urlAuth;
     private scopeForPersonal;
     private scopeForCorporation;
-    constructor(clientSecretKey: string, isIgnoreTSL?: boolean, isPersonal?: boolean, autoRefreshToken?: boolean);
+    constructor(clientSecretKey: string, isIgnoreTSL?: boolean, isPersonal?: boolean, autoRefreshToken?: boolean, imgOn?: boolean, imgPath?: string);
     private get;
     private post;
+    private getImage;
+    private extractImageSource;
     private handlingError;
     createToken(): Promise<any>;
     completion(data: any): Promise<any>;
--- index.js
+++ index.js
@@ -12,16 +12,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = require("axios");
 const uuid_1 = require("uuid");
 const https_1 = require("https");
+const fs = require("fs");
+const stream_1 = require("stream");
 class GigaChat {
-    constructor(clientSecretKey, isIgnoreTSL = true, isPersonal = true, autoRefreshToken = true) {
-        this.url = 'https://gigachat.devices.sberbank.ru/api/v1';
-        this.urlAuth = 'https://ngw.devices.sberbank.ru:9443/api/v2/oauth';
-        this.scopeForPersonal = 'GIGACHAT_API_PERS';
-        this.scopeForCorporation = 'GIGACHAT_API_CORP';
+    constructor(clientSecretKey, isIgnoreTSL = true, isPersonal = true, autoRefreshToken = true, imgOn = true, imgPath = ".") {
+        this.url = "https://gigachat.devices.sberbank.ru/api/v1";
+        this.urlAuth = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth";
+        this.scopeForPersonal = "GIGACHAT_API_PERS";
+        this.scopeForCorporation = "GIGACHAT_API_CORP";
         this.clientSecretKey = clientSecretKey;
         this.isIgnoreTSL = isIgnoreTSL;
         this.isPersonal = isPersonal;
         this.autoRefreshToken = autoRefreshToken;
+        this.imgOn = imgOn,
+            this.imgPath = imgPath;
     }
     get(path) {
         return __awaiter(this, void 0, void 0, function* () {
@@ -45,27 +49,50 @@ class GigaChat {
                 httpsAgent: new https_1.Agent({
                     rejectUnauthorized: !this.isIgnoreTSL
                 }),
-                responseType: stream ? 'stream' : 'json'
+                responseType: stream ? "stream" : "json"
             });
             return response;
         });
     }
+    getImage(imageId) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const responce = yield axios_1.default.get(`${this.url}/files/${imageId}/content`, {
+                headers: {
+                    Authorization: `Bearer ${this.authorization}`,
+                    Accept: "application/jpg"
+                },
+                httpsAgent: new https_1.Agent({
+                    rejectUnauthorized: !this.isIgnoreTSL
+                }),
+                responseType: "stream"
+            });
+            return responce;
+        });
+    }
+    extractImageSource(completionContent) {
+        const imgTagRegex = /<img[^>]+src\s*=\s*['"]([^'"]+)['"][^>]*>/;
+        const match = completionContent.match(imgTagRegex);
+        if (match) {
+            return match[1];
+        }
+        else {
+            return null;
+        }
+    }
     handlingError(error, currentFunction) {
         return __awaiter(this, void 0, void 0, function* () {
-            if (this.autoRefreshToken && error.response.data.message === 'Token has expired') {
+            if (this.autoRefreshToken && error.response.data.message === "Token has expired") {
                 try {
                     yield this.createToken();
                     const responce = yield currentFunction();
                     return responce.data;
                 }
                 catch (error) {
-                    console.error("GigaChat Error (create completion):\n", error);
-                    throw error;
+                    throw new Error(`GigaChat Error (create completion):\n${error}`);
                 }
             }
             else {
-                console.error("GigaChat Error (create completion):\n", error);
-                throw error;
+                throw new Error(error);
             }
         });
     }
@@ -75,16 +102,16 @@ class GigaChat {
                 const requestUID = (0, uuid_1.v4)();
                 const data = new URLSearchParams();
                 if (this.isPersonal) {
-                    data.append('scope', this.scopeForPersonal);
+                    data.append("scope", this.scopeForPersonal);
                 }
                 else {
-                    data.append('scope', this.scopeForCorporation);
+                    data.append("scope", this.scopeForCorporation);
                 }
                 const responce = yield axios_1.default.post(this.urlAuth, data, {
                     headers: {
-                        'Authorization': `Bearer ${this.clientSecretKey}`,
-                        'RqUID': requestUID,
-                        'Content-Type': 'application/x-www-form-urlencoded'
+                        "Authorization": `Bearer ${this.clientSecretKey}`,
+                        "RqUID": requestUID,
+                        "Content-Type": "application/x-www-form-urlencoded"
                     },
                     httpsAgent: new https_1.Agent({
                         rejectUnauthorized: !this.isIgnoreTSL
@@ -94,17 +121,53 @@ class GigaChat {
                 return responce.data;
             }
             catch (error) {
-                console.error('GigaChat Error (create authorization token):\n', error);
-                throw error;
+                throw new Error(`GigaChat Error (create authorization token):\n${error}`);
             }
         });
     }
     completion(data) {
         return __awaiter(this, void 0, void 0, function* () {
-            const path = '/chat/completions';
+            const path = "/chat/completions";
             try {
                 const response = yield this.post(path, data);
-                return response.data;
+                const completionContent = response.data.choices[0].message.content;
+                if (this.imgOn) {
+                    const imageId = this.extractImageSource(completionContent);
+                    if (imageId) {
+                        try {
+                            const imagePath = `${this.imgPath}/${(0, uuid_1.v4)()}.jpg`;
+                            const imageStream = fs.createWriteStream(imagePath);
+                            const transformStream = new stream_1.Readable();
+                            transformStream._read = () => { };
+                            const imageResponse = yield this.getImage(imageId);
+                            yield new Promise((resolve, reject) => {
+                                imageResponse.data.on("data", (chunk) => transformStream.push(chunk));
+                                imageResponse.data.on("end", () => {
+                                    transformStream.push(null);
+                                    transformStream.pipe(imageStream);
+                                    transformStream.on("end", () => {
+                                        imageStream.end();
+                                        imageStream.on("finish", () => {
+                                            response.data.choices[0].message["image"] = imagePath;
+                                            resolve();
+                                        });
+                                    });
+                                });
+                                imageResponse.data.on("error", reject);
+                            });
+                            return response.data;
+                        }
+                        catch (error) {
+                            throw new Error(`Ошибка при сохранении файла: ${error}`);
+                        }
+                    }
+                    else {
+                        return response.data;
+                    }
+                }
+                else {
+                    return response.data;
+                }
             }
             catch (error) {
                 return yield this.handlingError(error, () => __awaiter(this, void 0, void 0, function* () {
@@ -115,7 +178,7 @@ class GigaChat {
     }
     completionStream(data) {
         return __awaiter(this, void 0, void 0, function* () {
-            const path = '/chat/completions';
+            const path = "/chat/completions";
             try {
                 const response = yield this.post(path, data, true);
                 return response.data;
@@ -129,7 +192,7 @@ class GigaChat {
     }
     allModels() {
         return __awaiter(this, void 0, void 0, function* () {
-            const path = '/models';
+            const path = "/models";
             try {
                 const responce = yield this.get(path);
                 return responce.data;
@@ -157,3 +220,4 @@ class GigaChat {
     }
 }
 exports.default = GigaChat;
+//# sourceMappingURL=index.js.map
